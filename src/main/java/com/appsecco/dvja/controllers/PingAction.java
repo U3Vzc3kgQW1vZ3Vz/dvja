904533c5906e43fef775cb1ac339fc6c8678b96a
U3Vzc3kgQW1vZ3Vz <alibaba1232003@gmail.com>
2025-03-24 17:06:22 +0700

    added safe implementation of Command Injection vulnerable code

--- src/main/java/com/appsecco/dvja/controllers/PingAction.java
+++ src/main/java/com/appsecco/dvja/controllers/PingAction.java
@@ -1,10 +1,13 @@
 package com.appsecco.dvja.controllers;
 
+import com.appsecco.dvja.services.SafeModeService;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class PingAction extends BaseController {
 
@@ -43,7 +46,16 @@ public class PingAction extends BaseController {
     private void doExecCommand() throws IOException {
         Runtime runtime = Runtime.getRuntime();
 //        Command injection sink
-        String[] command = { "/bin/bash", "-c", "ping -t 5 -c 5 " + getAddress() };
+        if(SafeModeService.isSafe()){
+            try{
+                InetAddress ipAddress = InetAddress.getByName(getAddress());
+                setAddress(ipAddress.getHostAddress());
+            } catch (UnknownHostException e) {
+                setCommandOutput("Error running command: " + e.getMessage());
+                return;
+            }
+        }
+        String[] command = { "/bin/bash", "-c", "ping -c 5 " + getAddress() };
         Process process = runtime.exec(command);
 
         BufferedReader  stdinputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));

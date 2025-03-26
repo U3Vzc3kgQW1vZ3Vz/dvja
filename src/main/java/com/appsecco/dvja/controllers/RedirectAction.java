36f8adb088a0bb1de2afcb09c646212424ee6c23
U3Vzc3kgQW1vZ3Vz <alibaba1232003@gmail.com>
2025-03-26 10:20:32 +0700

    added URL validation in redirect URL in safe mode

--- src/main/java/com/appsecco/dvja/controllers/RedirectAction.java
+++ src/main/java/com/appsecco/dvja/controllers/RedirectAction.java
@@ -1,6 +1,10 @@
 package com.appsecco.dvja.controllers;
 
+import com.appsecco.dvja.services.SafeModeService;
 import org.apache.commons.lang.StringUtils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class RedirectAction extends BaseController {
 
@@ -15,10 +19,23 @@ public class RedirectAction extends BaseController {
     }
 
     public String execute() {
-        if(!StringUtils.isEmpty(getUrl()))
-            return "redirect";
 
-        return renderText("Missing url");
+        if(StringUtils.isEmpty(getUrl()))
+            return renderText("Missing url");
+
+        if(SafeModeService.isSafe()){
+            try{
+                URL resolvedUrl= new URL(getUrl());
+                if(!resolvedUrl.getHost().equalsIgnoreCase("localhost")){
+                    return renderText("Can't redirect to external page");
+                }
+            } catch (MalformedURLException e) {
+                if (getUrl().startsWith("/")||getUrl().startsWith("localhost"))
+                    return "redirect";
+                return renderText(e.getMessage());
+            }
+        }
+        return "redirect";
     }
 
 }
